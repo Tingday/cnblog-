5152958f69c11a8ad56c6bc947da01a289e8e598
Tinday <woyufan@163.com>
2020-03-08 00:52:04 +0800

    优化程序

--- main.py
+++ main.py
@@ -1,41 +1,64 @@
 # -*- coding:utf-8 -*-
-"""
-显然创建这个程序主要是为了偷懒
-同时希望能够把收集的博客文章，制作成epub电子书，方便浏览
+'''
+显然创建这个程序主要是为收藏博客的好文章
 联系我：woyufan@163.com
-"""
+'''
 from bs4 import BeautifulSoup
 import requests
 import os
 import sys
+import json
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
-def cnblog_content(cnblog_url):
-    # cnblog_url = "https://www.cnblogs.com/newcaoguo/p/7103249.html"
-    # cnblog_url = "https://www.cnblogs.com/steven_oyj/archive/2010/05/22/1741375.html"
+def cnblogs_content(cnblog_url):
+    '''
+    cnblog_url为文章地址
+    输入博客地址，返回博客内容 
+    '''
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " \
                  "Chrome/78.0.3904.108 Safari/537.36 "
     accept_language = "zh-CN,zh;q=0.9,en;q=0.8"
     authority = "www.cnblogs.com"
     headers = {"user_agent": user_agent, "accept-language": accept_language, "authority": authority}
+    # print("requests...")
     response = requests.get(cnblog_url, headers=headers)
-    # print(response.text)
-    # print(response.content)
-    soup = BeautifulSoup(response.content, features="html.parser")
-    # print(unicode(Soup))
-    cnblogs_post_body = unicode(soup.find("div", id="cnblogs_post_body"))
-    # get title
-    cb_post_title_url = unicode(soup.find("a", id="cb_post_title_url").get_text())
-    # save to html file
-    # print(cb_post_title_url)
-    fo = open(cb_post_title_url + ".html", "wb")
-    fo.write(cnblogs_post_body)
-    fo.close()
-    print(cb_post_title_url)
+    soup = BeautifulSoup(response.text, features="html.parser")
+    # 内容
+    cnblogs_post_body = soup.find("div", id="cnblogs_post_body")
+    cnblogs_post_body_text = cnblogs_post_body.get_text()
+    # 标题
+    cb_post_title_url = soup.find("a", id="cb_post_title_url").get_text()
+    cnblogs_data = {"title":str(cb_post_title_url), "body":str(cnblogs_post_body), "body_text":str(cnblogs_post_body_text), "html":response.text}
+    '''
+    # 关于unicode这里有两个
+    第一个是bs4会把内容统一使用unicode编码
+    另一个是python3 会把封装的dict也统一使用unicode编码。
+    如果需要字符串的方法显示，那么只需要把字典中特定的key返回即可
+    '''
+    return cnblogs_data
 
+def save_to_file(filename, txt):
+    '''
+    两个参数filenae和要保存的内容txt
+    无返回值
+    '''
+    #print(type(txt))
+    # print("saving...")
+    fo = open(filename, "w")
+    fo.write(txt)
+    fo.close()
 
 if __name__ == "__main__":
-    cnblog_content("https://www.cnblogs.com/steven_oyj/archive/2010/05/22/1741375.html")
+    cnblogs_data = cnblogs_content("https://www.cnblogs.com/extjs4/p/12381467.html")
+    save_to_file("articles/" + cnblogs_data["title"] + ".html", cnblogs_data["body"])
+    print("done.")
+    #print(json.dumps(cnblogs_data, ensure_ascii=False))
+
+
+
+
+
+
